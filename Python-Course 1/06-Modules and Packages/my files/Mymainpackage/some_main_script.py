86dbdccc79df3cd179de83db2829399293a6d32f
NavenduShekhar <56315418+NavenduShekhar@users.noreply.github.com>
2020-03-11 11:05:49 +0530

    notes

---
+++ Python-Course 1/06-Modules and Packages/my files/Mymainpackage/some_main_script.py
@@ -0,0 +1,2 @@
+def report_main():
+	print('Hey I am a function inside main script in main package')
---
+++ Python-Course 1/06-Modules and Packages/my files/myprogram.py
@@ -0,0 +1,6 @@
+from mymodule import my_func
+from Mymainpackage import some_main_script
+from Mymainpackage.Subpackage import my_sub_script
+my_func()
+some_main_script.report_main()
+my_sub_script.sub_report()
